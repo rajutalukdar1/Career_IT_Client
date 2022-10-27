ee9d78f1e7011db773344a77629f64b9d302dc57
rajutalukdar1 <lalutaluker@gmail.com>
2022-10-27 17:21:19 +0600

    firebase inti k env at convert

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBt5MxbmM6lrmzIuucFNKPxZH-5B1PY2NM",
-    authDomain: "assignment-no-10-b385a.firebaseapp.com",
-    projectId: "assignment-no-10-b385a",
-    storageBucket: "assignment-no-10-b385a.appspot.com",
-    messagingSenderId: "1080263695260",
-    appId: "1:1080263695260:web:b6019b6adc659576464659"
+    apiKey: process.env.REACT_APP_apikey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_torageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
